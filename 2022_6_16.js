f22c71a3d88e62e683a64f183ecf739875cf2e43
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-06-16 22:25:26 -0400

    Create 2022_6_16.js

---
+++ 2022_6_16.js
@@ -0,0 +1,8 @@
+/*Your task is to write a function that takes a string and return a new string with all vowels removed.
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".*/
+
+function disemvowel(str) {
+  let newString = str.replace(/[aeiou]/gi, "");
+  return newString;
+}
+
