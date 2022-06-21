405e890a741bbe394fe5dfb89842ad7a3fe08235
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-06-21 18:19:34 -0400

    Update 2022_6_21.js

--- 2022_6_21.js
+++ 2022_6_21.js
@@ -39,3 +39,25 @@ function DNAtoRNA(dna) {
   }
   return splitDna.join("");
 }
+
+
+/*3. Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language)
+that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+Examples (Input -> Output)
+
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5] */
+
+var min = function (list) {
+  let min = Math.min(...list);
+  return min;
+};
+
+var max = function (list) {
+  let max = Math.max(...list);
+  return max;
+};
+
+
