1950043864e78029d8765e6b1bae86b385b91211
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-06-20 22:18:40 -0400

    Update 2022_6_20.js

--- 2022_6_20.js
+++ 2022_6_20.js
@@ -30,3 +30,18 @@ function getMiddle(s) {
   }
   return result;
 }
+
+/* 2.Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot separating them.
+It should look like this:
+
+Sam Harris => S.H
+patrick feeney => P.F
+  */
+
+function abbrevName(name) {
+  let splitName = name.split(" ");
+  let fullInitial = [];
+  fullInitial.push(splitName[0][0], splitName[1][0]);
+  return fullInitial.join(".").toUpperCase();
+}
