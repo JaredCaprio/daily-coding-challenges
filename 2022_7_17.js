5f1a4df5809b389993740c24b1267cd308a98516
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-07-17 16:31:34 -0400

    Update 2022_7_17.js

--- 2022_7_17.js
+++ 2022_7_17.js
@@ -9,3 +9,30 @@ const binaryArrayToNumber = (arr) => {
   return parseInt(numArr, 2);
 };
 
+/* 2. Task Overview:
+You have to write a function that accepts three parameters:
+    cap is the amount of people the bus can hold excluding the driver.
+    on is the number of people on the bus excluding the driver.
+    wait is the number of people waiting to get on to the bus excluding the driver.
+If there is enough space, return 0, and if there isn't, return the number of passengers he can't take. */
+
+function enough(cap, on, wait) {
+  if (on + wait - cap <= 0) {
+    return 0;
+  } else {
+    return on + wait - cap;
+  }
+}
+
+/*3. Your task is to create the functionisDivideBy (or is_divide_by) to check 
+if an integer number is divisible by both integers a and b. */
+
+function isDivideBy(number, a, b) {
+  if (number % a === 0 && number % b === 0){
+    return true
+  } else {
+    return false
+  }
+}
+
+
