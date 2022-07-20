a094797979e5d720e810ce3eaf103435074bba05
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-07-19 23:18:52 -0400

    Update 2022_7_19.js

--- 2022_7_19.js
+++ 2022_7_19.js
@@ -6,3 +6,37 @@ String.prototype.isUpperCase = function () {
   return this == this.toUpperCase();
 };
 
+/* 2 You will be given a vector of strings. You must sort it alphabetically
+(case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+The returned value must be a string, and have "***" between each of its letters.
+You should not remove or add elements from/to the array.
+*/
+
+function twoSort(s) {
+  let result = s.sort();
+  return result[0].split("").join("***");
+}
+
+/*3. Given a set of numbers, return the additive inverse of each. Each positive 
+becomes negatives, and the negatives become positives.
+You can assume that all values are integers. Do not mutate the input array/list.
+ */
+const invert = array => array.map((num) => num >= 0 ? -Math.abs(num) : Math.abs(num));
+
+/* 4.  Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+function getCount(str) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let splitStr = str.split("");
+
+  let count = 0;
+  splitStr.filter((letter) => {
+    if ([...vowels].includes(letter) === true) {
+      count++;
+    }
+  });
+  return count;
+}
