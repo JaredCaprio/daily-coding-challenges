41196dcd3d4c4b928b04c496add14c91774ff546
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-08-27 11:54:16 -0400

    Create 2022_8_27.js

---
+++ 2022_8_27.js
@@ -0,0 +1,26 @@
+/*1. Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+    if x > y: 3 points
+    if x < y: 0 point
+    if x = y: 1 point
+
+Notes:
+
+    there are 10 matches in the championship
+    0 <= x <= 4
+    0 <= y <= 4
+
+*/
+function points(games) {
+  let count = 0;
+
+  for (let i = 0; i < games.length; i++) {
+    if (games[i][0] > games[i][2]) count += 3;
+    if (games[i][0] === games[i][2]) count += 1;
+  }
+  return count;
+}
