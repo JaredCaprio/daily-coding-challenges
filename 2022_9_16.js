8fbd0fdfd02c5e3b940a450680c003d5b4c2a5c0
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-09-16 13:06:26 -0400

    Create 2022_9_16.js

---
+++ 2022_9_16.js
@@ -0,0 +1,26 @@
+/* 1. Your Job
+
+Find the sum of all multiples of n below m
+Keep in Mind
+
+    n and m are natural numbers (positive integers)
+    m is excluded from the multiples
+
+Examples
+
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID"
+ */
+
+function sumMul(n, m) {
+  if (n <= 0 || m <= 0) return "INVALID";
+  let result = [];
+  for (let i = n; i < m; i += n) {
+    result.push(i);
+  }
+  return result.reduce((a, b) => {
+    return a + b;
+  }, 0);
+}
