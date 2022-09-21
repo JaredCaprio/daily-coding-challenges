499b05d16cb4961a12a5f05e5d27a5ed360d2cc0
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-09-21 17:50:59 -0400

    Create 2022_9_21.js

---
+++ 2022_9_21.js
@@ -0,0 +1,8 @@
+function rainAmount(mm) {
+  if (mm < 40) {
+  return `You need to give you plant ${40 - mm}mm of water`;
+} else {
+   return "Your plant has had more than enough water for today!";
+}
+
+}
