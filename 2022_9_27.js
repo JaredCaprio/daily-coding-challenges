4edd812a2a20a4e6154264b49edd0dc119c9a4b2
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-09-27 21:55:41 -0400

    Create 2022_9_27.js

---
+++ 2022_9_27.js
@@ -0,0 +1,21 @@
+/*1. Basic regex tasks. 
+Write a function that 
+takes in a numeric code 
+of any length. The function
+ should check if the code 
+begins with 1, 2, or 3 and 
+return true if so. Return
+ false otherwise.
+
+You can assume the input
+ will always be a number.
+*/
+
+function validateCode (code) {
+  let regex = /^[123]/;
+  if (regex.test(code)){
+    return true;
+  } else {
+    return false;
+  }
+}
