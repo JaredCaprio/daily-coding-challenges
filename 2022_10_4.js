57b10bede2d90c15b064708b8dfa9e205d417b46
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-10-04 11:31:48 -0400

    Create 2022_10_4.js

---
+++ 2022_10_4.js
@@ -0,0 +1,15 @@
+/*1. mission 1:
+
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+
+use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+
+type the console.log() in the next line (don't forget to put the str in the parentheses).*/
+function helloWorld (){
+  var str = "Hello World!";
+  console.log("Hello World!")
+}
