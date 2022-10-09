4a604fca682b98a65f76259068a59654e63167c4
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-10-09 13:19:11 -0400

    Create 2022_10_9.js

---
+++ 2022_10_9.js
@@ -0,0 +1,6 @@
+/*1. Write reverseList function that simply reverses lists.
+https://www.codewars.com/kata/57a04da9e298a7ee43000111/train/javascript
+*/
+function reverseList(arr) {
+  return arr.reverse();
+}
