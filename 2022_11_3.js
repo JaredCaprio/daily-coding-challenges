15026d6a3a1e1998fcb74e7fae5730ed2eaaacd5
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-11-03 12:07:13 -0400

    Update 2022_11_3.js

--- 2022_11_3.js
+++ 2022_11_3.js
@@ -20,7 +20,6 @@ In this Kata, you will be given a string and your task will be to return a list 
 
 Solve("*'&ABCDabcde12345") = [4,5,5,3]. 
 --the order is: uppercase letters, lowercase, numbers and special characters.
-
 More examples in the test cases. 
 */
 function solve(s) {
