35a96d857b34d458386a12369afe2328a454ad01
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-11-08 12:03:18 -0500

    Update 2022_11_8.js

--- 2022_11_8.js
+++ 2022_11_8.js
@@ -18,3 +18,26 @@ function sumOfDifferences(arr) {
   }
   return sum;
 }
+
+/*2.  https://www.codewars.com/kata/59706036f6e5d1e22d000016/train/javascript
+If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice as strong as love :-)
+
+Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+The input will always be made of only lowercase letters and will never be empty.
+
+*/
+function wordsToMarks(string) {
+  let sum = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    sum += string.charCodeAt(i) - 96;
+  }
+  return sum;
+}
