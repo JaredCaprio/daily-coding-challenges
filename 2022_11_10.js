ba77bce3572a1b163812ac68a6bc0dc2fbc270dd
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-11-10 08:52:43 -0500

    Create 2022_11_10.js

---
+++ 2022_11_10.js
@@ -0,0 +1,54 @@
+/* 1. https://www.codewars.com/kata/5d5ee4c35162d9001af7d699/train/javascript
+Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+For Example:
+
+[ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+, [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+, [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+]
+
+So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+Note: You will always be given a non-empty list containing positive values.
+
+ENJOY CODING :)
+*/
+function sumOfMinimums(arr) {
+  let sorted = [];
+  for (let i = 0; i < arr.length; i++) {
+    sorted.push(Math.min(...arr[i]));
+  }
+  return sorted.reduce((a, b) => a + b);
+}
+
+/* 2. https://www.codewars.com/kata/5300901726d12b80e8000498/train/javascript
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+    If the value is a multiple of 3: use the value "Fizz" instead
+    If the value is a multiple of 5: use the value "Buzz" instead
+    If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+
+N will never be less than 1.
+
+Method calling example:
+
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+*/ 
+function fizzbuzz(n) {
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
