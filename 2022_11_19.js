cb6f2729a76466eb282c6200664cba0734762f37
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-11-19 11:47:59 -0500

    Create 2022_11_19.js

---
+++ 2022_11_19.js
@@ -0,0 +1,26 @@
+/*1. https://www.codewars.com/kata/51b6249c4612257ac0000005/train/javascript
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer.
+You don't need to validate the form of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately,
+starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC)
+and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+*/
+function solution (s) {
+  const key = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+
+  let ans = [];
+
+  for (let i = 0; i < s.length; i++) {
+    console.log(key[s[i]]);
+    if (key[s[i]] < key[s[i + 1]]) {
+      // subtract it
+      ans.push(key[s[i + 1]] - key[s[i]]);
+      i++;
+      continue;
+    } else {
+      ans.push(key[s[i]]);
+    }
+  }
+  return ans.reduce((a, b) => a + b);
+}
