3587defb5e2d53b779464985a7feed023c999185
Jared <88853874+JaredCaprio@users.noreply.github.com>
2022-12-09 13:00:43 -0500

    Update 2022_12_5.js

--- 2022_12_5.js
+++ 2022_12_5.js
@@ -1,5 +1,7 @@
 /*1. https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/javascript
-You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
+The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+Write a method that takes the array as an argument and returns this "outlier" N.
 Examples
 
 [2, 4, 0, 100, 4, 11, 2602, 36]
